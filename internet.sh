f518773551ab95305df79b07924105479ff63d93
root <root@localhost.localdomain>
2016-12-15 22:28:39 -0600

    Forgot -t nat

--- internet.sh
+++ internet.sh
@@ -8,4 +8,4 @@ iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
-iptables -A POSTROUTING -o eth0 --jump MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 --jump MASQUERADE
